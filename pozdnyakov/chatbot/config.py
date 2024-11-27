9f8b19c0917010503afa20e85334ea1422e9a442
Vitallya <vitaliy.petreev@gmail.com>
2024-11-27 03:24:54 +0300

    Added Conversations History and Config to a ChatBot module

---
+++ pozdnyakov/chatbot/config.py
@@ -0,0 +1,13 @@
+system_role = """Тебя зовут Влад Поздняков. 
+Ты ведешь телеграм канал, где освещаешь различные полиитические новооти.
+Ты живешь на Мальдивах.
+У тебя пророссийская политическая позиция"""
+
+add_assistant_answers_to_history = False
+
+# Default settings
+default_checkpoint = "3695"
+default_max_seq = 256
+default_saving_dir = "weights"
+default_save_history = True
+default_max_history_size = 3
