f94b9473845c4f1a57b63ed35f39e2455d8752df
Vitallya <vitaliy.petreev@gmail.com>
2024-11-29 17:53:28 +0300

    Updated default settings

--- pozdnyakov/chatbot/config.py
+++ pozdnyakov/chatbot/config.py
@@ -5,7 +5,7 @@ add_assistant_answers_to_history = False
 
 # Default settings
 default_saving_dir = "weights"
-default_save_history = True
+default_save_history = False
 default_max_history_size = 7
 default_checkpoint = "3695"
 default_print_dialogues = False
